5538deb914bc1364fe1e15c9a4fd93f4cafdb53c
Oleksiy Lopatskiy <oleksiylopatskiy@MacBook-Pro-Oleksiy.local>
2020-10-19 16:11:13 +0300

    Fixed styles

--- Lab3.1/Lab3.1/main.cpp
+++ Lab3.1/Lab3.1/main.cpp
@@ -5,35 +5,38 @@
 // Варіант 9
 
 #include <iostream>
-   #include <cmath>
-using namespace std;
-int main(int argc, const char * argv[]) {
-    double x,y,A,B;
-    
-    cout<<"x=";cin >>x;
-    A=2*pow(abs(x), 3);
-    
-    if(x<=-0.1)
-        B=5*cos(18*x);
-    if(x>0.1&&x<1.2)
-        B=atan((x+2)/5);
-    if(x>=1.2)
-        B=cos(x+18);
-    
-    
-    y=A+B;
-    cout<<"1)"<<y;
-//----------------------------------------------------
-    
-    if(x<=-0.1)
-        B=5*cos(18*x);
-    else if (x>0.1&&x<1.2)
-        B=atan((x+2)/5);
-    else
-        B=cos(x+18);
-    
-    cout<<"1)"<<y;
 
-    cout << "Hello, World!\n";
-    return 0;
+#include <cmath>
+
+using namespace std;
+int main(int argc,
+  const char * argv[]) {
+  double x, y, A, B;
+
+  cout << "x=";
+  cin >> x;
+  A = 2 * pow(abs(x), 3);
+
+  if (x <= -0.1)
+    B = 5 * cos(18 * x);
+  if (x > 0.1 && x < 1.2)
+    B = atan((x + 2) / 5);
+  if (x >= 1.2)
+    B = cos(x + 18);
+
+  y = A + B;
+  cout << "1)" << y;
+  //----------------------------------------------------
+
+  if (x <= -0.1)
+    B = 5 * cos(18 * x);
+  else if (x > 0.1 && x < 1.2)
+    B = atan((x + 2) / 5);
+  else
+    B = cos(x + 18);
+
+  cout << "1)" << y;
+
+  cout << "Hello, World!\n";
+  return 0;
 }
